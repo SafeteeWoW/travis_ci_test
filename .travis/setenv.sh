e4fa8957044dd36fadbd10682a5494ce66b949a1
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2019-05-01 23:08:47 +0800

    Update setenv.sh

--- .travis/setenv.sh
+++ .travis/setenv.sh
@@ -1,6 +1,9 @@
 # Setup env vars which are determined at runtime.
 # Must source this script
 
+# Shouldn't use "set -eufo pipefail"
+# in the script which is sourced
+
 if [ -z "${PLATFORM:-}" ]; then
   export PLATFORM=$TRAVIS_OS_NAME;
 fi
@@ -22,3 +25,6 @@ mkdir -p $HOME/.lua
 export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${HOME}/install/luarocks/bin
 export LUA_HOME_DIR=$HOME/install/$LUA
 export LR_HOME_DIR=$HOME/install/luarocks
+
+mkdir -p "${LUA_HOME_DIR}"
+mkdir -p "${LR_HOME_DIR}"
