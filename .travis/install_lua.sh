d2b78ba9a5198496ea5521b3666e166a70ff4718
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2019-05-01 22:53:59 +0800

    Update

--- .travis/install_lua.sh
+++ .travis/install_lua.sh
@@ -2,16 +2,17 @@
 # install an implementation of Lua interpreter
 
 set -eufo pipefail
-cd "$(dirname "$0")"
 mkdir -p $HOME/install
 export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${HOME}/install/luarocks/bin
 export LUA_HOME_DIR=$HOME/install/$LUA
 export LR_HOME_DIR=$HOME/install/luarocks
 
+cd ${TRAVIS_BUILD_DIR}
+
 if [[ "$LUA" =~ luajit.* ]]; then
-	bash .travis/install_lua/luajit.sh
+	bash ${TRAVIS_BUILD_DIR}/.travis/install_lua/luajit.sh
 elif [[ "$LUA" =~ lua5.* ]]; then
-	bash .travis/install_lua/lua.sh
+	bash ${TRAVIS_BUILD_DIR}/.travis/install_lua/lua.sh
 else
 	echo "Don't know how to install $LUA"
 	exit 1
